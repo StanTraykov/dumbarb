8b38ff5f2a05be2a70a595509372462b91dcd83d
StanTraykov <36573153+StanTraykov@users.noreply.github.com>
2018-03-29 05:56:23 +0300

    0.2.0

--- dumbarb.py
+++ dumbarb.py
@@ -25,7 +25,7 @@ import datetime, os, shlex, string, subprocess, sys, time
 # config constants -- you may want to change these:
 
 DUMBARB = 'dumbarb'
-DUMBVER = '0.1.5'
+DUMBVER = '0.2.0'
 
 FMT_PRERESULT = '[{seqno:0{swidth}}] {name1} {col1} {name2} {col2} = '
 FMT_WIN_W = '{name:>{nwidth}} W+'
